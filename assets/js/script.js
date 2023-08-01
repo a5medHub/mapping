f1705a21531ae478ad1fbfabed270373c20e78f9
A.Yassine <ayassine.auce@gmail.com>
2023-08-01 19:22:38 +0300

    testing

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let username = ['admin01','admin02','admin03']
+
+console.log(username)
+username.push('admin04')
+username.push('admin05')
+console.log(username)
+username.pop()
+username.pop()     
+console.log(username)
+username.unshift('admin00')
+console.log(username)
+username.shift()
+console.log(username)
