3728a7a582a99fea3fbb15650d2de086d8332204
A.Yassine <ayassine.auce@gmail.com>
2023-08-01 20:33:19 +0300

    make function works

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
-let username = ['admin01','admin02','admin03']
-
-console.log(username)
-username.push('admin04')
-username.push('admin05')
-console.log(username)
-username.pop()
-username.pop()     
-console.log(username)
-username.unshift('admin00')
-console.log(username)
-username.shift()
-console.log(username)
+const addBtnEl = document.getElementById('addBtn')
+const userListEl = document.getElementById('userList')
+const liEl = document.createElement('li')
+const addToListEl = document.getElementById('addToList')
+let username = []
+// username.push('admin04')
+// username.pop() 
+// username.unshift('admin00')
+// username.shift()
+// console.log(username)
+addBtnEl.addEventListener('click', function(){
+    username = []
+    let newInput = addToListEl.value
+        username.push(newInput)
+    for (let i in username){
+        userListEl.innerHTML += `<li>${username[i]}</li>`
+    }
+    addToListEl.value = ''
+    console.log(username)
+})
